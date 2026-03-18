a39365167e8ebf0f78b94d7b0a7fd3436ffd231c
Unnati Tomar <tunnati87@gmail.com>
2026-03-18 10:10:06 +0530

    Add simple interest calculation script
    Implement simple interest calculation based on user input.

---
+++ simple interest.py
@@ -0,0 +1,8 @@
+# to calculate simple interest.
+p = float(input("Enter Principal: "))
+r = float(input("Enter Rate: "))
+t = float(input("Enter Time: "))
+
+si = (p * r * t) / 100
+
+print("Simple Interest =", si)
